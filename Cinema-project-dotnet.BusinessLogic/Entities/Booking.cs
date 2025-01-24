fe7e02bb8494f67c9eebb6341297b66c000fad64
Олег Дробіт <olegdrobit16@gmail.com>
2025-01-24 13:19:56 +0200

    User ID changed to non-nullable

--- Cinema-project-dotnet.BusinessLogic/Entities/Booking.cs
+++ Cinema-project-dotnet.BusinessLogic/Entities/Booking.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string? UserId { get; set; }
+        public string UserId { get; set; }
 
-        public User? User { get; set; }
+        public User User { get; set; }
 
         public int SessionId { get; set; }
 
